bbbf2c140b0cc6351d8b4fc7f4e084117249b114
Santiago Montoya <santiago.mordz@gmail.com>
2024-09-12 02:55:25 +0000

    conversiones terminadas

---
+++ app.js
@@ -0,0 +1,30 @@
+/*const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}*/
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+function fromDollarToYen (dollar) {
+    let dollarToEuro = dollar / oneEuroIs.USD 
+    return dollarToEuro * oneEuroIs.JPY;
+}
+
+function fromEuroToDollar (euro) {
+    return euro * oneEuroIs.USD;
+}
+
+function fromYenToPound (yen) {
+    let yenToEuro = yen / oneEuroIs.JPY;
+    return yenToEuro * oneEuroIs.GBP;
+}
+
+// We include fromEuroToDollar here as well because it needs to be exported
+module.exports = { fromEuroToDollar }
